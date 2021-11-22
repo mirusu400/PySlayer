dd9ba8ac2469500294dae72483f3ecc8d1c98818
mirusu400 <mirusu400@naver.com>
2021-11-22 23:25:24 +0900

    Update csnsocket

--- lib/csnsocket.py
+++ lib/csnsocket.py
@@ -6,14 +6,14 @@ import binascii
 
 class CSNSocket:
     def __new__(cls, *args, **kwargs):
-        if not hasattr(cls, "_instance"):         # Foo 클래스 객체에 _instance 속성이 없다면
+        if not hasattr(cls, "_instance"): 
             print("CSNsocket::__new__ is called")
-            cls._instance = super().__new__(cls)  # Foo 클래스의 객체를 생성하고 Foo._instance로 바인딩
-        return cls._instance                      # Foo._instance를 리턴
+            cls._instance = super().__new__(cls)
+        return cls._instance
 
     def __init__(self):
         cls = type(self)
-        if not hasattr(cls, "_init"):             # Foo 클래스 객체에 _init 속성이 없다면
+        if not hasattr(cls, "_init"):
             print("CSNsocket::__init__ is called")
 
             # seqnum is just the order that packet is sent/receive.
